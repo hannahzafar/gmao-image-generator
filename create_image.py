bfe629271f620412e8c358925ec8c2a2e21a1970
Hannah Zafar <hannah.zafar@nasa.gov>
2026-01-06 17:27:26 -0500

    Note tiling options

--- create_image.py
+++ create_image.py
@@ -3,7 +3,12 @@
 from hybig import create_browse
 
 # geotiff_path = "micasa-cog/monthly/2001/MiCASA_v1_NEE_x3600_y1800_monthly_200101.tif"
-geotiff_path = "test-images/MiCASA_v1_NEE_x3600_y1800_monthly_200101.tif"
+# geotiff_folder = "test-images/test-image-one/"
+geotiff_folder = "test-images/test-image-tiles/"
+geotiff_path = (
+    geotiff_folder + "MiCASA_v1_NEE_x3600_y1800_monthly_200101.tif"
+)  # The TIF has to be in the folder where the browse imagery will be created
+
 results = create_browse(
     geotiff_path,
     {
@@ -13,6 +18,7 @@ results = create_browse(
             "x": {"min": -180, "max": 180},
             "y": {"min": -90, "max": 90},
         },
+        # "scale_size": {"x": 10, "y": 10},
         "scale_size": {"x": 0.005, "y": 0.005},
     },
 )
